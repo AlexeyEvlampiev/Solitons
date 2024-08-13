793067fe22534567cf41d1f77462a007b91b3fd6
Alexey <alexey.evlampiev@gmail.com>
2024-08-13 19:26:34 +0200

    wip

--- test/Solitons.Core.XUnitTest/CommandLine/CliProcessor_Should.cs
+++ test/Solitons.Core.XUnitTest/CommandLine/CliProcessor_Should.cs
@@ -1,12 +1,18 @@
-﻿using Moq;
+﻿using System;
+using Moq;
 using Xunit;
 
 namespace Solitons.CommandLine;
 
 public class CliProcessor_Should
 {
-    [Fact]
-    public void Work()
+    [Theory]
+    [InlineData("test-cli", 1, 1)]
+    [InlineData("test-cli -?", 0, 1)]
+    [InlineData("test-cli -h", 0, 1)]
+    [InlineData("test-cli --help", 0, 1)]
+    [InlineData("test-cli run", 0, 0)]
+    public void Work(string commandLine, int expectedExitCode, int showHelpCalledCount)
     {
         var callback = new Mock<ICliProcessorCallback>();
         
@@ -14,17 +20,21 @@ public class CliProcessor_Should
             .Setup(options => options
                 .UseCommandsFrom(this)
                 .UseCallback(callback.Object));
+        
+        var showHelpCalled = showHelpCalledCount switch
+        {
+            0 => Times.Never(),
+            1 => Times.Once(),
+            _ => throw new NotSupportedException()
+        };
 
-        int exitCode = processor.Process("tool");
-        callback.Verify(m => m.ShowHelp("tool"), Times.Once);
-        Assert.Equal(1, exitCode);
-
-        exitCode = processor.Process("tool -?");
-        callback.Verify(m => m.ShowHelp("tool"), Times.Once);
-        Assert.Equal(0, exitCode);
+        int exitCode = processor.Process(commandLine);
+        callback.Verify(m => m.ShowHelp(It.IsAny<string>()), showHelpCalled);
+        callback.Verify(m => m.ShowHelp(commandLine), showHelpCalled);
+        Assert.Equal(expectedExitCode, exitCode);
     }
 
-    [CliCommand("test")]
+    [CliCommand("run")]
     public void TestAction()
     {
 
