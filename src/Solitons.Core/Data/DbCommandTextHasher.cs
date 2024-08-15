2b23415eac8657cfeb82dbe6d94dd85448b43967
Alexey <alexey.evlampiev@gmail.com>
2024-08-15 21:17:45 +0200

    wip

---
+++ src/Solitons.Core/Data/DbCommandTextHasher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Solitons.Data;
+
+public sealed class DbCommandTextHasher : IDisposable
+{
+    private readonly SHA256 _crypto;
+    private readonly Regex _regex;
+    private int _disposed = 0;
+
+    public DbCommandTextHasher()
+    {
+        _crypto = SHA256.Create();
+        var pattern = @"(?xis-m)(?:@text|@comment|\s+)"
+            .Replace("@text", @"(?<text>""[^""]*"")|(?<text>'[^']*')")
+            .Replace("@comment", @"--[^\n]*\n|/[*].*?[*]/");
+        _regex = new Regex(pattern);
+    }
+
+    public string ComputeHash(string commandText)
+    {
+        if (Thread.VolatileRead(ref _disposed) == 1)
+        {
+            throw new ObjectDisposedException(
+                nameof(DbCommandTextHasher), 
+                $"The {typeof(DbCommandTextHasher)} instance has been disposed and cannot be used.");
+        }
+        commandText = _regex.Replace(commandText, m =>
+        {
+            if (m.Groups["text"].Success)
+            {
+                return m.Value;
+            }
+
+            return string.Empty;
+        });
+        var hashHex = _crypto
+            .ComputeHash(Encoding.UTF8.GetBytes(commandText))
+            .Select(b => b.ToString("x2"))
+            .Join(string.Empty);
+        return hashHex;
+    }
+
+    void IDisposable.Dispose()
+    {
+        if (0 == Interlocked.CompareExchange(ref _disposed, 1, 0))
+        {
+            _crypto.Dispose();
+        }
+    }
+}
---
+++ test/Solitons.Core.XUnitTest/Data/DbCommandTextHasher_ComputeHash_Should.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Solitons.Data;
+
+public sealed class DbCommandTextHasher_ComputeHash_Should
+{
+    [Theory]
+    [InlineData(@"
+        SELECT * FROM public.test
+    ")]
+    [InlineData(@"
+        -- This is a test
+        SELECT * FROM public.test
+    ")]
+    [InlineData(@"
+        SELECT * FROM public.test
+        /*
+            This is a test
+        */
+    ")]
+    [InlineData(@"
+        -- This is a test
+        SELECT * FROM public.test
+        /*
+            This is a test
+        */
+    ")]
+    public void Work(string command)
+    {
+        using var alg = new DbCommandTextHasher();
+        var expectedHash = alg.ComputeHash("SELECT * FROM public.test");
+        var actualHash = alg.ComputeHash(command);
+        Assert.Equal(expectedHash, actualHash);
+    }
+}
