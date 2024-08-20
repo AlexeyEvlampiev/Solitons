730cfc4d35bf2df81c1a84d3568bfec3f91e52a6
Alexey <alexey.evlampiev@gmail.com>
2024-08-20 21:08:45 +0200

    wip

---
+++ src/Solitons.Core/CommandLine/CliCommandExampleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Solitons.CommandLine;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class CliCommandExampleAttribute : Attribute
+{
+    public CliCommandExampleAttribute(string example)
+    {
+        Example = example;
+    }
+
+    public string Example { get; }
+}
--- src/Solitons.Postgres.PgUp/Program.cs
+++ src/Solitons.Postgres.PgUp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Reactive;
+using Npgsql;
 using Solitons.CommandLine;
 
 namespace Solitons.Postgres.PgUp;
@@ -9,11 +10,11 @@ public class Program
 {
     private static readonly TimeSpan DefaultActionTimeout = TimeSpan.FromMinutes(10);
 
-    public const string DeployCommandDescription = "Deploys postgres database following to the pgup.json deployment plan.";
+    public const string DeployCommandDescription = "Deploys a PostgreSQL database according to the pgup.json deployment plan, ensuring all configurations and resources are correctly applied.";
     public const string InitializeProjectCommand = "init|initialize";
     public const string ProjectDirectoryArgumentDescription = "File directory where to initialize the new pgup project.";
-    public const string InitializeProjectCommandDescription = "Creates a new pgup project structure in the specified directory.";
-    public const string TemplateParameterDescription = "The project template to be used.";
+    public const string InitializeProjectCommandDescription = "Initializes a new PgUp project structure in the specified directory, setting up the necessary files and folders for PostgreSQL deployment.";
+    public const string TemplateParameterDescription = "Specifies the project template to be applied during initialization.";
 
     private readonly IPgUpTemplateRepository _templates = new PgUpFileSystemTemplateRepository();
 
@@ -24,28 +25,52 @@ public class Program
             .Setup(config => config
                 .UseCommandsFrom<Program>()
                 .UseLogo(PgUpResource.AsciiLogo)
-                .UseDescription("Postgres database deployment tool."))
+                .UseDescription("CLI for managing PostgreSQL deployments and tasks using a structured, user-defined sequence of database transactions."))
             .Process();
     }
 
     [CliCommand("deploy")]
-    [CliArgument(nameof(projectFile), "PgUp project file.")]
+    [CliArgument(nameof(projectFile), "PgUp configuration file.")]
     [Description(DeployCommandDescription)]
+    [CliCommandExample("deploy pgup.json --host localhost --username %ADMIN_USR% --password %ADMIN_PWD%")]
     public static Task<int> DeployAsync(
         string projectFile,
-        [CliOption("--host")] string host,
-        [CliOption("--user")] string username,
-        [CliOption("--password")] string password,
+        ConnectionBuilderBundle connection,
         [PgUpParametersOption] Dictionary<string, string>? parameters = null,
-        [CliOption("--timeout")] CancellationToken cancellation = default)
+        [CliOption("--timeout")] TimeSpan? timeout = null)
     {
-        return PgUpManager.DeployAsync(
-            projectFile,
-            host,
-            username, 
-            password,
-            parameters ?? [],
-            cancellation);
+        return PgUpManager
+            .DeployAsync(
+                projectFile,
+                connection.ToString(),
+                false,
+                false,
+                parameters ?? [],
+                timeout ?? DefaultActionTimeout);
+    }
+
+
+    [CliCommand("deploy")]
+    [CliArgument(nameof(projectFile), "PgUp configuration file.")]
+    [Description(DeployCommandDescription)]
+    [CliCommandExample("deploy pgup.json --host localhost --username %ADMIN_USR% --password %ADMIN_PWD% --overwrite")]
+    [CliCommandExample("deploy pgup.json --host localhost --username %ADMIN_USR% --password %ADMIN_PWD% --overwrite --force")]
+    public static Task<int> DeployAsync(
+        string projectFile,
+        ConnectionBuilderBundle connection,
+        [CliOption("--overwrite")] Unit overwrite,
+        [CliOption("--force")] Unit? forceOverride = null,
+        [PgUpParametersOption] Dictionary<string, string>? parameters = null,
+        [CliOption("--timeout")] TimeSpan? timeout = null)
+    {
+        return PgUpManager
+            .DeployAsync(
+                projectFile,
+                connection.ToString(),
+                true,
+                forceOverride.HasValue,
+                parameters ?? [],
+                timeout ?? DefaultActionTimeout);
     }
 
 
@@ -91,13 +116,40 @@ public class Program
                 timeout ?? DefaultActionTimeout);
     }
 
-    sealed class PgUpParametersOptionAttribute : CliOptionAttribute, ICliMapOption
+    sealed class PgUpParametersOptionAttribute()
+        : CliOptionAttribute("--parameter|-p", "Defines parameters for customizing deployment scripts."), ICliMapOption
     {
-        public PgUpParametersOptionAttribute() 
-            : base("--parameter|-p", "PgUp parameters to be substituted in all scripts")
-        {
-        }
-
         public StringComparer GetComparer() => StringComparer.OrdinalIgnoreCase;
+    }
+
+    public sealed class ConnectionBuilderBundle : CliOptionBundle
+    {
+        [CliOption("--host", "Specifies the PostgreSQL server hostname or IP address.")]
+        public string Host { get; set; } = "localhost";
+
+        [CliOption("--port", "Specifies the port number on which the PostgreSQL server is listening.")]
+        public int Port { get; set; } = 5432;
+
+        [CliOption("--maintenance-database|--mdb", "The name of the maintenance database used for administrative tasks, typically postgres.")]
+        public string MaintenanceDatabase { get; set; } = "postgres";
+
+        [CliOption("--username|--user|-usr|-u", "The username to connect to the PostgreSQL maintenance database.")]
+        public string Username { get; set; } = "postgres";
+
+        [CliOption("--password|-pwd", "The password associated with the specified PostgreSQL user.")]
+        public string Password { get; set; } = "postgres";
+
+        public override  string ToString()
+        {
+            return new NpgsqlConnectionStringBuilder()
+            {
+                Host = Host,
+                Port = Port,
+                Database = MaintenanceDatabase,
+                Username = Username,
+                Password = Password
+            }
+            .ConnectionString;
+        }
     }
 }
