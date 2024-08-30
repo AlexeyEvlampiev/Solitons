820070f061ab60eb91c4c47d0c7ce2f43b9ba168
Alexey <alexey.evlampiev@gmail.com>
2024-08-30 08:44:44 +0200

    wip

---
+++ src/Solitons.Core/CommandLine/CliActionRegularExpressionRtt.custom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solitons.CommandLine;
+
+internal partial class CliActionRegularExpressionRtt
+{
+    public static readonly string UnrecognizedToken = $"unrecognized_token_{typeof(CliActionRegularExpressionRtt).GUID:N}";
+    internal CliActionRegularExpressionRtt(CliActionSchema schema)
+    {
+        var segments = schema
+            .CommandSegments
+            .ToArray();
+
+        CommandSegmentRegularExpressions = segments
+            .Select((segment, index) => segment.IsArgument
+                ? segment.BuildRegularExpression()
+                : $"(?<{GenGroupName(index)}>{segment.BuildRegularExpression()})")
+            .ToArray();
+
+        OptionRegularExpressions = schema
+            .Options
+            .Select(option => option.BuildRegularExpression())
+            .ToArray();
+    }
+
+    private string GenGroupName(int index) => $"segment_{GetType().GUID:N}_{index}";
+
+    private IReadOnlyList<string> CommandSegmentRegularExpressions { get; }
+    public IReadOnlyList<string> OptionRegularExpressions { get; }
+}
