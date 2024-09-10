290d11a97d248e7603f710f5007a37ebcf4d2723
Alexey <alexey.evlampiev@gmail.com>
2024-09-10 22:25:34 +0200

    wip

--- test/Solitons.Core.XUnitTest/CommandLine/CliOptionInfo_ctor_Should.cs
+++ test/Solitons.Core.XUnitTest/CommandLine/CliOptionInfo_ctor_Should.cs
@@ -9,13 +9,13 @@ namespace Solitons.CommandLine;
 public sealed class CliOptionInfo_ctor_Should
 {
     [Theory]
-    //[InlineData(123, "Description goes here", true, 321)]
-    //[InlineData(null, "Another description", false, 564)]
-    //[InlineData(null, "Should handle default value", false, null)] // Default value should be used
-    //[InlineData(999, "Using default value if no match", false, null)] // No match, default should be used
+    [InlineData(123, "Description goes here", true, 321)]
+    [InlineData(null, "Another description", false, 564)]
+    [InlineData(null, "Should handle default value", false, null)] // Default value should be used
+    [InlineData(999, "Using default value if no match", false, null)] // No match, default should be used
     [InlineData(-1, "Negative values allowed", true, -1)] // Test negative value
-    //[InlineData(int.MaxValue, "Max int value", true, int.MaxValue)] // Test extreme positive value
-    //[InlineData(int.MinValue, "Min int value", true, int.MinValue)] // Test extreme negative value
+    [InlineData(int.MaxValue, "Max int value", true, int.MaxValue)] // Test extreme positive value
+    [InlineData(int.MinValue, "Min int value", true, int.MinValue)] // Test extreme negative value
     public void HandleIntValueProperty(int? defaultValue, string description, bool isRequired, int? value)
     {
         var metadata = new Mock<ICliOptionMetadata>();
@@ -30,12 +30,13 @@ public sealed class CliOptionInfo_ctor_Should
         Assert.Equal(isRequired, target.IsRequired);
 
         string input = value?.ToString() ?? ""; // Convert value to string, handle null cases
-        var match = Regex.Match(input, $@"^(?<{target.RegexMatchGroupName}>\d+)?\S*$"); 
-        if (match.Success && value.HasValue)
+        var match = Regex.Match(input, $@"^(?<{target.RegexMatchGroupName}>\d+)?.*$");
+        var group = match.Groups[target.RegexMatchGroupName];
+        if (group.Success && value.HasValue)
         {
             Assert.Equal(value, target.Deserialize(match, key => key)); 
         }
-        else if (!match.Success && isRequired)
+        else if (!group.Success && isRequired)
         {
             Assert.Throws<CliExitException>(() => target.Deserialize(match, key => key));
         }
@@ -44,6 +45,5 @@ public sealed class CliOptionInfo_ctor_Should
             // If no match and not required, should return the default value
             Assert.Equal(defaultValue, target.Deserialize(match, key => key));
         }
-
     }
 }
