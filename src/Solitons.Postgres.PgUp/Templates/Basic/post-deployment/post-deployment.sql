8bb843cab44604f43256ad2ce5d8162813dafc23
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2024-12-25 19:23:37 +0100

    wip

--- src/Solitons.Postgres.PgUp/Templates/Basic/post-deployment/post-deployment.sql
+++
@@ -1,9 +0,0 @@
-
-/*
-  This template guides the creation of post-deployment scripts, typically used for seeding reference data or similar tasks. 
-  While these scripts are usually idempotent, you can configure them for single or repeated execution through 'pgup.json'.
-
-  Place scripts in the post-deployment directory, ensuring they are named for easy identification and ordered as needed.
-
-  Update 'pgup.json' to manage how and when these scripts are run.
-*/
--- src/Solitons.Postgres.PgUp/templates/basic/migrations/migration.00-00-00.sql
+++
@@ -1,7 +0,0 @@
-/*
-  This template is for one-time execution migration scripts. Add SQL commands required for initial deployment.
-
-  Organize scripts in the migrations directory by predefined order, clearly naming each file.
-
-  Update 'pgup.json' to control the execution sequence.
-*/
