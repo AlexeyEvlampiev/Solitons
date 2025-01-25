a96b232bd37c856c39808fb9b487050453d81a6c
Alexey <alexey.evlampiev@gmail.com>
2025-01-25 15:58:16 +0100

    wip

--- src/Solitons.Postgres.PgUp.Native/build.ps1
+++ src/Solitons.Postgres.PgUp.Native/build.ps1
@@ -1,4 +1,17 @@
-﻿cd "c:\Users\alexey.evlampiev\source\repos\AlexeyEvlampiev\Solitons\src\Solitons.Postgres.PgUp.Native\"
-cls
+﻿# Get the directory of the script
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Path
+
+# Change to the script's directory
+Set-Location -Path $scriptDirectory
+
+# Clear the screen
+Clear-Host
+
+# Publish for Windows (win-x64)
 dotnet publish -c Release -r win-x64   --self-contained true /p:PublishSingleFile=true
-#dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
+
+# Publish for Linux (linux-x64)
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
+
+# Publish for macOS (osx-x64)
+dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishSingleFile=true
